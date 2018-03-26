796b7df72dce0e5322f39cd271038d5a5d3bfd2f
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2018-03-26 13:04:56 +0300

    getting worked

--- main.cpp
+++ main.cpp
@@ -2,11 +2,41 @@
 #include <list>
 #include "stack.h"
 #include <vector>
+#include <ctime>
+#include <iomanip>
+
+std::vector<std::pair<int, int> > generateSequence(size_t size) {
+    std::vector<std::pair<int, int> > result;
+//    for (size_t i = 0; i < size; i++) {
+    size_t curSize = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (curSize > 0 && rand() % 2) {
+            result.emplace_back(std::make_pair(-1, -1));
+        } else
+            result.emplace_back(std::make_pair(1, rand() % 100500));
+    }
+    return result;
+};
+
+template<class alloc>
+double doTesting(const std::vector<std::pair<int, int> >& res) {
+    std::list<int, alloc> kekos;
+//    auto res = generateSequence(10000);
+    clock_t t = clock();
+    for (auto& el: res) {
+        if (el.first == -1) {
+            kekos.pop_back();
+        } else
+            kekos.emplace_back(el.second);
+    }
+    t = clock() - t;
+    return double(t)/CLOCKS_PER_SEC;
+}
 
 int main() {
-//    std::vector<int, StackAllocator<int> > kekos;
-//    std::list<int, StackAllocator<int> > kek;
-    StackAllocator<int> kekos;
-//    std::cout << "Hello, World!" << std::endl;
+//    srand(time(0));
+    auto result = generateSequence(10000000);
+    std::cout << doTesting<StackAllocator<int> >(result) << std::endl;
+    std::cout << doTesting<std::allocator<int> >(result) << std::endl;
     return 0;
 }
--- stack.h
+++ stack.h
@@ -1,27 +1,31 @@
 #include <iostream>
+#include <cassert>
 
-template<class T>
+
 class AllocationArea {
 private:
     template<class>
     friend class StackAllocator;
 //    friend class StackAllocator<T>;
 
-    typedef T value_type;
-    typedef T* pointer;
-    typedef const T* const_pointer;
-    typedef T& reference;
-    typedef const T& const_reference;
+//    typedef T value_type;
+//    typedef T* pointer;
+//    typedef const T* const_pointer;
+//    typedef T& reference;
+//    typedef const T& const_reference;
 
     static const size_t MAX_ALLOCATION = 1000000;
 
-    pointer _cur;
+//    pointer _cur;
+    char* _cur;
     size_t _available;
-    AllocationArea<T>* _nextArea;
+//    AllocationArea<T>* _nextArea;
+    AllocationArea* _nextArea;
 
     bool _checkAvailability(size_t requiredMemory);
 
-    pointer _allocate(size_t requiredMemory);
+//    pointer _allocate(size_t requiredMemory);
+    char* _allocate(size_t requiredMemory);
 
 public:
     AllocationArea();
@@ -32,13 +36,15 @@ public:
 template<class T>
 class StackAllocator {
 private:
-    typedef AllocationArea<T>* area_pointer;
-    typedef AllocationArea<T> area;
+//    typedef AllocationArea<T>* area_pointer;
+//    typedef AllocationArea<T> area;
+    typedef AllocationArea* area_pointer;
+    typedef AllocationArea area;
 
     area_pointer _start;
     area_pointer _cur;
 
-    area_pointer _create_node();
+    area_pointer _createNode();
 
 public:
     typedef T value_type;
@@ -56,50 +62,83 @@ public:
         typedef StackAllocator<T1> other;
     };
 
-    pointer allocate(size_t elementNumber);
+    pointer allocate(size_t requiredMemory);
 
     void deallocate(pointer ptr, size_t size) {}
+
+//    void construct(pointer ptr, const_reference value);
 };
 
 //template class StackAllocator<int>;
 
 
-template<class T>
-AllocationArea<T>::AllocationArea() : _cur(reinterpret_cast<pointer>(new char[MAX_ALLOCATION])), _available(MAX_ALLOCATION), _nextArea(nullptr) {}
+//template<class T>
+//AllocationArea<T>::AllocationArea() : _cur((pointer) ::operator new(MAX_ALLOCATION)), _available(MAX_ALLOCATION), _nextArea(nullptr) {}
 
-template<class T>
-bool AllocationArea<T>::_checkAvailability(size_t requiredMemory) {
-    return MAX_ALLOCATION >= requiredMemory;
+AllocationArea::AllocationArea() : _cur(new char[MAX_ALLOCATION]), _available(MAX_ALLOCATION), _nextArea(nullptr) {}
+
+//template<class T>
+//bool AllocationArea<T>::_checkAvailability(size_t requiredMemory) {
+//    return _available >= requiredMemory;
+//}
+
+bool AllocationArea::_checkAvailability(size_t requiredMemory) {
+    return _available >= requiredMemory;
 }
 
+/**
 template<class T>
 typename AllocationArea<T>::pointer AllocationArea<T>::_allocate(size_t requiredMemory) {
-    assert(!_checkAvailability(requiredMemory));
+    assert(_checkAvailability(requiredMemory));
+    _available -= requiredMemory;
+    _cur += requiredMemory;
+    return _cur - requiredMemory;
+}
+ **/
+
+char* AllocationArea::_allocate(size_t requiredMemory) {
+    assert(_checkAvailability(requiredMemory));
+    _available -= requiredMemory;
     _cur += requiredMemory;
     return _cur - requiredMemory;
 }
 
-template<class T>
-AllocationArea<T>::~AllocationArea() {
-    delete[] (_cur + _available - MAX_ALLOCATION);
 
-}
+//template<class T>
+//AllocationArea<T>::~AllocationArea() {
+//    delete[] (_cur + _available - MAX_ALLOCATION);
+//}
 
 template<class T>
-typename StackAllocator<T>::area_pointer StackAllocator<T>::_create_node() {
-    return _cur->_nextArea = new area();
+typename StackAllocator<T>::area_pointer StackAllocator<T>::_createNode() {
+    return _cur = _cur->_nextArea = new area();
 }
 
 template <class T>
-typename StackAllocator<T>::pointer StackAllocator<T>::allocate(size_t elementNumber) {
-    size_t requiredMemory = elementNumber * sizeof(T);
+typename StackAllocator<T>::pointer StackAllocator<T>::allocate(size_t requiredMemory) {
+    requiredMemory *= sizeof(T);
     if (!_cur->_checkAvailability(requiredMemory))
-        _create_node();
-    return _cur->_allocate(requiredMemory);
+        _createNode();
+//    return _cur->_allocate(requiredMemory);
+    return reinterpret_cast<pointer>(_cur->_allocate(requiredMemory));
 }
 
+
+AllocationArea::~AllocationArea() {
+    delete [] (_cur + _available - MAX_ALLOCATION);
+}
+
+
+//template<class T>
+//void StackAllocator<T>::construct(pointer ptr, const_reference value) {
+//    new((void*)ptr) value_type(value);
+//}
+
+//template<class T>
+//StackAllocator<T>::StackAllocator(): _start(new AllocationArea<T>()), _cur(_start) {}
+
 template<class T>
-StackAllocator<T>::StackAllocator(): _start(new AllocationArea<T>()), _cur(_start) {}
+StackAllocator<T>::StackAllocator() : _start(new AllocationArea()), _cur(_start) {}
 
 template<class T>
 StackAllocator<T>::~StackAllocator() {
